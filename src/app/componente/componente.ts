2382eb5446ec275bf864d699dd88acc5a588310a
Jorge Daniel Silva <daniel1jorge@hotmail.com>
2022-01-05 14:20:11 -0300

    Comienzo de aprendizaje, se agrega componente.

---
+++ src/app/componente/componente.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'hello-world',
+    templateUrl: './componente.html',
+    styleUrls: ['./componente.css']
+})
+export class HelloWorld{
+
+}
